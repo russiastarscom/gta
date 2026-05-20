b5283685a4fd40f50f2800a884dff2460915879c
Twiin <grigorirock1999@gmail.com>
2026-05-20 18:11:18 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+var CACHE_NAME='city-drive-v1';
+var ASSETS=[
+  './',
+  './index.html',
+  './manifest.json'
+];
+self.addEventListener('install',function(e){
+  e.waitUntil(caches.open(CACHE_NAME).then(function(cache){return cache.addAll(ASSETS)}));
+  self.skipWaiting();
+});
+self.addEventListener('activate',function(e){
+  e.waitUntil(caches.keys().then(function(names){
+    return Promise.all(names.filter(function(n){return n!==CACHE_NAME}).map(function(n){return caches.delete(n)}));
+  }));
+  self.clients.claim();
+});
+self.addEventListener('fetch',function(e){
+  e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}));
+});
